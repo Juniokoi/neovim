438a40eb4901e6fa56d97eeaf5158e82ec953ef6
Junio Koi <53125029+Juniokoi@users.noreply.github.com>
2023-01-13 22:35:30 -0300

    feat: remove require dependencies

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,7 +1,15 @@
-require("load").packages { path = "core", packages = {
-    "settings",
-    "packer",
-    "lisp",
-    "snip",
-}}
+function Set_Leader_Key()
+	vim.keymap.set("", "<Space>", "<Nop>")
+	vim.g.mapleader = " "
+	vim.g.maplocalleader = " "
+end
 
+
+function load_core()
+	Set_Leader_Key()
+	require("core.lazy")
+	require("keymaps.keymaps")
+	require("core.settings")
+end
+
+load_core()
